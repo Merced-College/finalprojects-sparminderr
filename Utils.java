49111809ef9414078e0241523e6981a627d9d57d
sparminderr <sparminderr@gmail.com>
2026-04-24 03:24:29 +0000

    algorithms

--- Utils.java
+++ Utils.java
@@ -3,5 +3,13 @@
 
 public class Utils {
 
-    
+    // recurse algorithm, if n is reached method ends.
+    // time complexity O(n)
+    public static void countdown(int n) {
+        if (n == 0)
+            return;
+        System.out.println(n);
+        countdown(n - 1);
+    }
+
 }
